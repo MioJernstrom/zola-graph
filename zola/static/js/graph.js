5509aa5c9e9d6dec56cdb42efbee85f88c9ce3bb
AmandaJernstrom <61943279+AmandaJernstrom@users.noreply.github.com>
2022-05-11 22:09:42 +0200

    Graph

--- zola/static/js/graph.js
+++ zola/static/js/graph.js
@@ -78,6 +78,23 @@ var options = {
 			},
 		},
 	},
+	layout: {
+		randomSeed: undefined,
+		improvedLayout:true,
+		clusterThreshold: 150,
+		hierarchical: {
+		  enabled:false,
+		  levelSeparation: 150,
+		  nodeSpacing: 100,
+		  treeSpacing: 200,
+		  blockShifting: true,
+		  edgeMinimization: true,
+		  parentCentralization: true,
+		  direction: 'UD',        // UD, DU, LR, RL
+		  sortMethod: 'hubsize',  // hubsize, directed
+		  shakeTowards: 'leaves'  // roots, leaves
+		}
+	  },
 	edges: {
 		color: { inherit: "both" },
 		width: 0.8,
