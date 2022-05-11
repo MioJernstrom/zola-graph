bdf8db2b921b51406cf65f84113494de3e47310f
AmandaJernstrom <61943279+AmandaJernstrom@users.noreply.github.com>
2022-05-11 22:24:41 +0200

    Update graph.js

--- zola/static/js/graph.js
+++ zola/static/js/graph.js
@@ -78,22 +78,54 @@ var options = {
 			},
 		},
 	},
-	layout: {
-		randomSeed: undefined,
-		improvedLayout:true,
-		clusterThreshold: 150,
-		hierarchical: {
-		  enabled:true,
-		  levelSeparation: 150,
-		  nodeSpacing: 1000,
-		  treeSpacing: 500,
-		  blockShifting: true,
-		  edgeMinimization: true,
-		  parentCentralization: true,
-		  direction: 'UD',        // UD, DU, LR, RL
-		  sortMethod: 'hubsize',  // hubsize, directed
-		  shakeTowards: 'leaves'  // roots, leaves
-		}
+	physics:{
+		enabled: true,
+		barnesHut: {
+		  theta: 0.5,
+		  gravitationalConstant: -2000,
+		  centralGravity: 0.3,
+		  springLength: 95,
+		  springConstant: 0.04,
+		  damping: 0.09,
+		  avoidOverlap: 0
+		},
+		forceAtlas2Based: {
+		  theta: 0.5,
+		  gravitationalConstant: -50,
+		  centralGravity: 0.01,
+		  springConstant: 0.08,
+		  springLength: 100,
+		  damping: 0.4,
+		  avoidOverlap: 0
+		},
+		repulsion: {
+		  centralGravity: 0.2,
+		  springLength: 200,
+		  springConstant: 0.05,
+		  nodeDistance: 100,
+		  damping: 0.09
+		},
+		hierarchicalRepulsion: {
+		  centralGravity: 0.0,
+		  springLength: 100,
+		  springConstant: 0.01,
+		  nodeDistance: 120,
+		  damping: 0.09,
+		  avoidOverlap: 0
+		},
+		maxVelocity: 50,
+		minVelocity: 0.1,
+		solver: 'barnesHut',
+		stabilization: {
+		  enabled: true,
+		  iterations: 1000,
+		  updateInterval: 100,
+		  onlyDynamicEdges: false,
+		  fit: true
+		},
+		timestep: 0.5,
+		adaptiveTimestep: true,
+		wind: { x: 0, y: 0 }
 	  },
 	edges: {
 		color: { inherit: "both" },
